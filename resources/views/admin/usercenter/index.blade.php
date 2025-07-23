400374344a7b8918534cf90dbfef80572dfc95ce
huanglican <lican.huang.hz@gmail.com>
2025-07-23 10:19:54 +0800

    user center update

---
+++ resources/views/admin/usercenter/index.blade.php
@@ -0,0 +1,30 @@
+{{--
+@copyright (c) 2025 Hangzhou Domain Zones Technology Co., Ltd., Institute of Future Science and Technology G.K., Tokyo
+@author Lican Huang
+@created 2025-07-23
+* License: Dual Licensed – GPLv3 or Commercial
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* As an alternative to GPLv3, commercial licensing is available for organizations
+* or individuals requiring proprietary usage, private modifications, or support.
+*
+* Contact: yvsoucom@gmail.com
+* GPL License: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="min-h-screen flex bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
+    </div>
+@endsection
