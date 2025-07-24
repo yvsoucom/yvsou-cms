8ef3a43939e4661e9348bd027600d004cbf24103
huanglican <lican.huang.hz@gmail.com>
2025-07-25 04:50:10 +0800

    fix reuse

--- routes/toggle.php
+++ routes/toggle.php
@@ -1,27 +1,15 @@
 <?php
+// SPDX-FileCopyrightText: 2025 Hangzhou Domain Zones Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2025 Institute of Future Science and Technology G.K., Tokyo
+// SPDX-FileContributor: Lican Huang
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Proprietary
+
 /**
- * © 2025 Hangzhou Domain Zones Technology Co., Ltd., Institute of Future Science and Technology G.K., Tokyo   All rights reserved.
- * Author: Lican Huang
- * 
- * SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Proprietary
- * License: Dual Licensed – GPLv3 or Commercial
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * As an alternative to GPLv3, commercial licensing is available for organizations
- * or individuals requiring proprietary usage, private modifications, or support.
- *
- * Contact: yvsoucom@gmail.com
- * GPL License: https://www.gnu.org/licenses/gpl-3.0.html
+ * This program is dual-licensed under GPLv3 or a commercial license.
+ * See the GPLv3 license at: https://www.gnu.org/licenses/gpl-3.0.html
+ * For commercial use, contact: yvsoucom@gmail.com
  */
+
 
 use App\Http\Controllers\Admin\PluginController;
 use App\Http\Controllers\ProfileController;
