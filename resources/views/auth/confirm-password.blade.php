9f36049e3853022ecc5781f26514b7726f44d013
huanglican <lican.huang.hz@gmail.com>
2025-07-25 04:39:33 +0800

    fix reuse

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,28 +1,15 @@
-{{--
-  @copyright (c) 2025  Hangzhou Domain Zones Technology Co., Ltd., Institute of Future Science and Technology G.K., Tokyo
-  @author Lican Huang
-  @created 2025-06-26
-*
-* SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Proprietary  
-* License: Dual Licensed – GPLv3 or Commercial
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* As an alternative to GPLv3, commercial licensing is available for organizations
-* or individuals requiring proprietary usage, private modifications, or support.
-*
-* Contact: yvsoucom@gmail.com
-* GPL License: https://www.gnu.org/licenses/gpl-3.0.html
-*/
---}}
+<?php
+// SPDX-FileCopyrightText: 2025 Hangzhou Domain Zones Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2025 Institute of Future Science and Technology G.K., Tokyo
+// SPDX-FileContributor: Lican Huang
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Proprietary
+
+/**
+ * This program is dual-licensed under GPLv3 or a commercial license.
+ * See the GPLv3 license at: https://www.gnu.org/licenses/gpl-3.0.html
+ * For commercial use, contact: yvsoucom@gmail.com
+ */
+?>
  <x-guest-layout>
     <div class="flex h-screen items-center justify-center bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
         <div class="w-full max-w-md p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md transition-colors duration-200">
--- resources/views/vendor/trumbowyg-svg.blade.php
+++ resources/views/vendor/trumbowyg-svg.blade.php
@@ -1,2 +1,13 @@
+<?php
+// SPDX-FileCopyrightText: 2025 Hangzhou Domain Zones Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2025 Institute of Future Science and Technology G.K., Tokyo
+// SPDX-FileContributor: Lican Huang
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Proprietary
 
+/**
+ * This program is dual-licensed under GPLv3 or a commercial license.
+ * See the GPLv3 license at: https://www.gnu.org/licenses/gpl-3.0.html
+ * For commercial use, contact: yvsoucom@gmail.com
+ */
+?>
 {!! file_get_contents(public_path('icons/trumbowyg/icons.svg')) !!}
