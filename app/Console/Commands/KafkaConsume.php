130cd65a2d9d9664aac6ef0955a424e03cd18029
huanglican <lican.huang.hz@gmail.com>
2025-08-03 16:40:38 +0800

    kafka

---
+++ app/Console/Commands/KafkaConsume.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* SPDX-FileCopyrightText: (c) 2025  Hangzhou Domain Zones Technology Co., Ltd.
+* SPDX-FileCopyrightText: Institute of Future Science and Technology G.K., Tokyo
+* SPDX-FileContributor: Lican Huang
+* @created 2025-08-03
+*
+* SPDX-License-Identifier: GPL-3.0-or-later
+* License: Dual Licensed – GPLv3 or Commercial
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* As an alternative to GPLv3, commercial licensing is available for organizations
+* or individuals requiring proprietary usage, private modifications, or support.
+*
+* Contact: yvsoucom@gmail.com
+* GPL License: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+// app/Console/Commands/KafkaConsume.php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\KafkaConsumerService;
+
+class KafkaConsume extends Command
+{
+    protected $signature = 'kafka:consume {topic}';
+    protected $description = 'Start Kafka consumer for given topic';
+
+    public function handle(KafkaConsumerService $consumerService)
+    {
+        $topic = $this->argument('topic');
+        $this->info("Listening to Kafka topic: {$topic}");
+        $consumerService->consume($topic);
+    }
+}
