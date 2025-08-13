18dc6ec63156c5ccd1ca313b42d86ab42f905b59
huanglican <lican.huang.hz@gmail.com>
2025-08-13 19:57:03 +0800

    version 2 beta 1

--- config/version.php
+++ config/version.php
@@ -25,7 +25,7 @@
 // config/version.php
 return [
   'github_repo' => 'yvsoucom/yvsou-cms',
-  'app_version' => 'v2.0.0-dev.10',
+  'app_version' => 'v2.0.0-beta.1',
   'env_version' => 'env-v1.0.0',
   'config_version' => 'config-v1.0.0',
   'sql_version' => 'sql-v1.2.0',
